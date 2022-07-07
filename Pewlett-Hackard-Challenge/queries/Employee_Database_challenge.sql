a74080805b3f35074c81d8595836c07c0357f476
Laura Miller <laura.shewmon@gmail.com>
2022-07-07 00:31:23 -0400

    queries complete

--- Pewlett-Hackard-Challenge/queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Challenge/queries/Employee_Database_challenge.sql
@@ -1,5 +1,6 @@
 -- Employees_Database_challenge.sql
 
+-- RETIRMENT TITLES QUERY
 SELECT e.emp_no, e.first_name, e.last_name, t.title, 
     t.from_date, t.to_date
 INTO retirement_titles
@@ -9,7 +10,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
+-- UNIQUE TITLES QUERY
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
@@ -19,8 +20,22 @@ FROM retirement_titles as rt
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no ASC, to_date DESC;
 
+-- RETIRING TITLES QUERY
 SELECT COUNT(title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+--MENTORSHIP ELIGIBILITY QUERY
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name,
+    e.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+    ON (e.emp_no = de.emp_no)
+    INNER JOIN titles AS ti
+    ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
